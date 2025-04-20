77c449510cd8fe96af69af4acd026aa07bfff0dd
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-04-20 15:01:20 +0530

    servers updated

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,9 +1,27 @@
 import {WebSocketServer} from "ws"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { JWT_SECRET } from "./config/config"
 
 const wss=new WebSocketServer({port:8080})
 
-wss.on('connection',(ws)=>{
-    ws.on('message',(data)=>{
-        ws.send('pongggggg')
+wss.on('connection',(ws,request) => {
+    const url=request.url;
+
+    if (!url) {
+        return ;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ?? "";
+    const decoded = jwt.verify(token,JWT_SECRET) as JwtPayload;
+
+    if (!decoded || !decoded.userId) {
+        ws.close();
+        return;
+    }
+
+    ws.on('message',(data) => {
+        ws.send('pong');
     })
+
 })
